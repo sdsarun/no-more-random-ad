46be868cc8fc2a6ad0181d6cd8cc645bb790548c
Sarun Daunghirun <125266560+sdsarun@users.noreply.github.com>
2025-04-08 13:42:00 +0700

    feat(random-thai-national-id): add copy options

--- components/featues/random-thai-national-id.tsx
+++ components/featues/random-thai-national-id.tsx
@@ -3,20 +3,27 @@
 import React, { useEffect, useState } from 'react'
 
 // components
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import Code from '@/components/ui/code';
+import CopyButton from '@/components/common/copy-button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Loading } from '@/components/ui/loading';
+import { Button } from '@/components/ui/button';
+import { Shuffle } from 'lucide-react';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Label } from '@/components/ui/label';
 
 // utils
 import randomThaiNationalID from '@/utils/random-thai-national-id';
-import CopyButton from '@/components/common/copy-button';
-import { Button } from '@/components/ui/button';
-import { Shuffle } from 'lucide-react';
 
 export type RandomThaiNationalIDProps = any;
 
+type CopyOptions = {
+  copyWithFormat: boolean;
+}
+
 export default function RandomThaiNationalID({ }: RandomThaiNationalIDProps) {
   const [thaiNationalID, setThaiNationalID] = useState<string>("");
+  const [copyOptions, setCopyOptioins] = useState<CopyOptions>({ copyWithFormat: false });
 
   useEffect(() => {
     setThaiNationalID(randomThaiNationalID());
@@ -32,21 +39,32 @@ export default function RandomThaiNationalID({ }: RandomThaiNationalIDProps) {
         {!thaiNationalID ? (
           <Loading className="w-full" size="sm" />
         ) : (
-          <>
-            <div className="w-full">
-              <Code>{thaiNationalID}</Code>
+          <div className='flex-1 flex flex-col gap-2'>
+            <div className='flex items-center justify-between gap-2'>
+              <div className="w-full">
+                <Code>{thaiNationalID}</Code>
+              </div>
+              <div className="flex items-center gap-2">
+                <CopyButton
+                  className="w-1/2 min-w-[106px]"
+                  copyContent={{ content: copyOptions.copyWithFormat ? thaiNationalID : thaiNationalID.replaceAll("-", ""), contentType: "text/plain" }}
+                />
+                <Button className="w-1/2 min-w-[106px]" variant={"outline"} onClick={() => setThaiNationalID(randomThaiNationalID())}>
+                  <Shuffle />
+                  Generate
+                </Button>
+              </div>
             </div>
-            <div className="flex items-center gap-2">
-              <CopyButton
-                className="w-1/2 min-w-[106px]"
-                copyContent={{ content: thaiNationalID, contentType: "text/plain" }}
+            <div className='flex items-center gap-1'>
+              <Checkbox
+                id='copy-with-format'
+                name='copy-with-format'
+                checked={copyOptions.copyWithFormat}
+                onCheckedChange={(checked) => setCopyOptioins((prevOptions) => ({ ...prevOptions, copyWithFormat: checked as boolean }))}
               />
-              <Button className="w-1/2 min-w-[106px]" variant={"outline"} onClick={() => setThaiNationalID(randomThaiNationalID())}>
-                <Shuffle />
-                Generate
-              </Button>
+              <Label htmlFor='copy-with-format'>Copy with format</Label>
             </div>
-          </>
+          </div>
         )}
       </CardContent>
     </Card>
