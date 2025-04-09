045dad5d89dddec4551113a9deb73a69afc4d2c0
Sarun Daunghirun <sdsarun@outlook.com>
2025-04-09 22:00:47 +0700

    style(random-uuid): add responsive

--- components/featues/random-uuid.tsx
+++ components/featues/random-uuid.tsx
@@ -26,12 +26,12 @@ export default function RandomUUID({ rootClassName }: RandomUUIDProps) {
   }, []);
 
   return (
-    <Card className={cn("min-w-[766px] min-h-[154px]", rootClassName)}>
+    <Card className={cn("md:min-w-[766px] md:min-h-[154px]", rootClassName)}>
       <CardHeader>
         <CardTitle>UUID v4</CardTitle>
         <CardDescription>Your Version 4 UUID:</CardDescription>
       </CardHeader>
-      <CardContent className="flex items-center justify-between gap-2">
+      <CardContent className="flex flex-col items-center md:flex-row md:justify-between gap-2">
         {!uuid ? (
           <Loading className="w-full" size="sm" />
         ) : (
@@ -39,12 +39,12 @@ export default function RandomUUID({ rootClassName }: RandomUUIDProps) {
             <div className="w-full">
               <Code>{uuid}</Code>
             </div>
-            <div className="flex items-center gap-2">
+            <div className="w-full md:w-auto flex items-center gap-2">
               <CopyButton
-                className="w-1/2 min-w-[106px]"
+                className="flex-1 md:w-1/2 md:min-w-[106px]"
                 copyContent={{ content: uuid, contentType: "text/plain" }}
               />
-              <Button className="w-1/2 min-w-[106px]" variant={"outline"} onClick={() => setUUID(crypto.randomUUID())}>
+              <Button className="flex-1 md:w-1/2 md:min-w-[106px]" variant={"outline"} onClick={() => setUUID(crypto.randomUUID())}>
                 <Shuffle />
                 Generate
               </Button>
